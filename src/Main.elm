c304ccec5dfde44b4b3a94d22a76935e581e8c29
Dennis Snell <dennis.snell@automattic.com>
2017-10-12 21:31:25 -0400

    Add basic routing structure

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,97 @@
 module Main exposing (..)
 
-import Html exposing (Html, div)
+import Html exposing (Html, div, text)
+import Navigation exposing (Location, program)
+import UrlParser exposing (Parser, map, oneOf, parseHash, stringParam, top, (<?>))
 
 
 type Msg
-    = NoOp
+    = UrlChange Location
+
+
+type LoadingStatus
+    = Idle
+    | Loading
+
+
+type Route
+    = Boot
+    | NoSiteGiven
+    | Site String
+    | SiteNotFound
 
 
 type alias Model =
-    {}
+    { networkStatus : LoadingStatus
+    , route : Route
+    }
 
 
 model : Model
 model =
-    {}
+    { networkStatus = Idle
+    , route = Boot
+    }
 
 
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = model
-        , view = view
+    program UrlChange
+        { init = init
+        , subscriptions = always Sub.none
         , update = update
+        , view = view
         }
 
 
-update : Msg -> Model -> Model
+init : Location -> ( Model, Cmd Msg )
+init location =
+    ( { model | route = parseRoute location }, Cmd.none )
+
+
+parseRoute : Location -> Route
+parseRoute =
+    parseHash routeParser >> Maybe.withDefault SiteNotFound
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    let
+        siteRoute s =
+            s
+                |> Maybe.map
+                    (\s ->
+                        if String.isEmpty s then
+                            NoSiteGiven
+                        else
+                            Site s
+                    )
+                |> Maybe.withDefault Boot
+    in
+        oneOf
+            [ map siteRoute (top <?> stringParam "site")
+            , map Boot top
+            ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model
+    case msg of
+        UrlChange location ->
+            ( { model | route = parseRoute location }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    case model.route of
+        Boot ->
+            div [] []
+
+        NoSiteGiven ->
+            div [] [ text "Add a site in the URL query string: e.g. '?site=design.blog'" ]
+
+        Site site ->
+            div [] []
+
+        SiteNotFound ->
+            div [] []
